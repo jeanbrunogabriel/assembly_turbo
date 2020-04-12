59959187ea12441ce5668602895ec3f62c80c58c
Jean Braga <jeanbrunogabriel@gmail.com>
2020-04-12 15:10:09 -0300

    1.0

--- autoGenerateASMexec.sh
+++ autoGenerateASMexec.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-
+#Variável que guarda o nome sem a extensão .asm
 EXEC_FILE="$(echo $1 | sed 's/.\{4\}$//')"
-echo $EXEC_FILE
+
+#Variável com a extensão .o
 OUT_FILE=$EXEC_FILE.o
-echo $OUT_FILE
+
+#Cria o arquivo .o (output)
 nasm -f elf32 $1
+
+#Cria arquivo executável (EXEC_FILE)
 ld -m elf_i386 -o $EXEC_FILE $OUT_FILE
+
+#Executa o arquivo criado
+./$EXEC_FILE
---
+++ hello_world.asm
@@ -0,0 +1,21 @@
+; ssize_t write(int fd, const void *buf, size_t count);
+
+section .data
+mensagem: db "HELLO", 0xa
+tamanho: equ $ - mensagem
+
+section .text
+global _start
+
+_start: 
+
+mov edx, tamanho; count 
+mov ecx, mensagem ; *buf
+mov ebx, 1 ; fd
+mov eax, 4 ; 
+int 0x80 
+
+; return 42x00
+mov ebx, 0 ; valor do syscal exit()
+mov eax, 1 ; syscal exit()
+int 0x80
